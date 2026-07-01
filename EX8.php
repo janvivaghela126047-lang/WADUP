05d0ffe61416226b71a3bddb6d7bf6632e61795b
janvivaghela126047-lang <janvi.vaghela126047@marwadiuniversity.ac.in>
2026-07-01 07:32:16 +0530

    Add files via upload

---
+++ EX8.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Reverse Array</title>
+</head>
+<body>
+
+<h2>Reverse Array Values</h2>
+
+<form method="post">
+    Enter array values (separated by comma):<br><br>
+    <input type="text" name="numbers" required>
+    <br><br>
+    <input type="submit" value="Reverse">
+</form>
+
+<?php
+if(isset($_POST['numbers']))
+{
+    $input = $_POST['numbers'];
+
+    // Convert string to array
+    $array = explode(",", $input);
+
+    // Reverse the array
+    $reverse = array_reverse($array);
+
+    echo "<h3>Original Array:</h3>";
+    echo implode(", ", $array);
+
+    echo "<h3>Reversed Array:</h3>";
+    echo implode(", ", $reverse);
+}
+?>
+
+</body>
+</html>
